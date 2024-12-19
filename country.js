bf45ea41d781c1396e44fd83f03bce600c022ec1
Himanshu Kumar Tiwari <153666762+darkworldprogrammer@users.noreply.github.com>
2024-12-19 19:01:44 +0530

    commited

--- country.js
+++ country.js
@@ -3,7 +3,7 @@ const countryContainer = document.querySelector(".country-details");
 const flagImg = document.querySelector(".flag");
 const countryNames = document.querySelector(".country-name");
 const textFirst = document.querySelector(".text-first");
-const textSecond = document.querySelector(".p");
+const textSecond = document.querySelector(".borders");
 const nativeName = document.querySelector(".nativename");
 const populationText = document.querySelector("#population");
 const population = document.querySelector(".population");
@@ -21,19 +21,23 @@ const languageText = document.querySelector("#language");
 const language = document.querySelector(".language");
 const borderCountriesText = document.querySelector(".border-country");
 const borderCountryH2 = document.querySelector(".h2");
-const border1 = document.querySelector(".border1");
-const border2 = document.querySelector(".border2");
-const border3 = document.querySelector(".border3");
+// const border1 = document.querySelector(".border1");
+// const border2 = document.querySelector(".border2");
+// const border3 = document.querySelector(".border3");
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   .then((res) => {
     return res.json();
   })
   .then(([country]) => {
-    console.log(country);
-
+    // console.log(country);
     flagImg.src = country.flags.svg;
     countryNames.innerText = country.name?.common;
+    if (!country.borders) {
+      const a =
+        (borderCountriesText.innerHTML = `<h3>ohh! ${country.name?.common} has no any border country..</h3>`);
+      borderCountriesText.style.color = `red`;
+    }
 
     if (country.name.nativeName) {
       nativeName.innerText = Object.values(country.name.nativeName)[0].official;
@@ -77,59 +81,43 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     } else {
       textSecond.removeChild(languageText);
     }
-    if (!country.borders) {
-      borderCountriesText.removeChild(borderCountryH2);
-      borderCountriesText.removeChild(textSecond);
-    }
-    if (country.borders[0]) {
-      border1.innerText = country.borders[0];
-    } else {
-      textSecond.removeChild(border1);
-    }
-    if (country.borders[1]) {
-      border2.innerText = country.borders[1];
-    } else {
-      textSecond.removeChild(border2);
-    }
-    if (country.borders[2]) {
-      border3.innerText = country.borders[2];
-    } else {
-      textSecond.removeChild(border3);
-    }
-  
+    // if (!country.borders) {
+    //   borderCountriesText.removeChild(borderCountryH2);
+    //   borderCountriesText.removeChild(textSecond);
+    // }
+
+    // if (country.borders[0]) {
+    //   border1.innerText = country.borders[0];
+    // } else {
+    //   textSecond.removeChild(border1);
+    // }
+    // if (country.borders[1]) {
+    //   border2.innerText = country.borders[1];
+    // } else {
+    //   textSecond.removeChild(border2);
+    // }
+    // if (country.borders[2]) {
+    //   border3.innerText = country.borders[2];
+    // }
+    // else {
+    //   textSecond.removeChild(border3);
+    // }
+    
+    country.borders.forEach((border)=>{
+      fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+      .then((res)=>res.json())
+      .then(([countriesBorders])=>{
+        // console.log(countriesBorders.name);
+        const borderCountriesTag = document.createElement('a')
+        borderCountriesTag.classList.add('border')
+        console.log(countriesBorders.name);
+        borderCountriesTag.innerText = countriesBorders.name.common
+        borderCountriesTag.href =`/country.html?name=${countriesBorders.name.common}`
+        textSecond.append(borderCountriesTag)
+      })
+    })
+
   })
   .catch((err) => {
     console.dir(err);
   });
-
-//////////////=======
-// countryContainer.innerHTML = `
-//                    <img class="flag" src="${country.flags.svg}" alt="" />
-//     <div class="details-text">
-//       <h1 class="country-name">${country.name?.common}</h1>
-//       <div class="all-details">
-//         <div>
-//           <p><b>Native name: </b>${country.name.nativeName?.eng?.official}</p>
-//           <p><b>population: </b>${country.population.toLocaleString()}</p>
-//           <p><b>region: </b>${country.region}</p>
-//           <p><b>sub region: </b>${country.subregion}</p>
-//           <p><b>capital: </b>${country.capital}</p>
-//         </div>
-//         <div>
-//           <p><b>Top Level Domain: </b>${country.tld}</p>
-//           <p><b>Currencies: </b>${country.currencies?.INR?.symbol}</p>
-//           <p><b>Languages: </b>${country.languages.eng}</p>
-//         </div>
-//       </div>
-//       <div class="border-country">
-//         <h2>Border Countries:</h2>
-//         <div class="p">
-//           <p>${country.borders[0]}</p>
-//           <p>${country.borders[1]}</p>
-//           <p>${country.borders[2]}</p>
-//           <p>${country.borders[3]}</p>
-//           <p>${country.borders[4]}</p>
-//         </div>
-//       </div>
-//     </div>
-// `;
